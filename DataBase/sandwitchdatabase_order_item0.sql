9b2b5243db00628a987a1c34d478e7ec2ca91341
SebastianSzczypkowski <65536856+SebastianSzczypkowski@users.noreply.github.com>
2021-06-07 12:10:51 +0200

    Update sandwitchdatabase_order_item0.sql

--- DataBase/sandwitchdatabase_order_item0.sql
+++ DataBase/sandwitchdatabase_order_item0.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `order_item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `order_item` (
-  `id_orderItem` int NOT NULL,
+  `id_order_item` int NOT NULL,
   `quantity` int NOT NULL,
   `unit_price` decimal(19,2) NOT NULL,
   `order_id` bigint NOT NULL,
