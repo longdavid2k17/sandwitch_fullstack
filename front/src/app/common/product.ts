d8ad4a93ef399d923b5b8e5d1b5854f02b50ccb5
SebastianSzczypkowski <szczsebastian146@gmail.com>
2021-06-07 20:16:00 +0200

    frontned-version-0.1

---
+++ front/src/app/common/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    id:number=0;
+    name:string='';
+    description:string='';
+    unitPrice:number=0;
+    imageUrl:string='';
+    available:boolean=false;
+    
+}
---
+++ front/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product=new Product();
+
+  constructor(private produtcService:ProductService,private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(()=> {this.handleProductDetails();})
+  }
+  handleProductDetails()
+  {
+    const theProductId: number=+this.route.snapshot.paramMap.get('id')!;
+    this.produtcService.getProduct(theProductId).subscribe(
+      data=>{
+        this.product=data;
+      }
+    )
+  }
+
+}
