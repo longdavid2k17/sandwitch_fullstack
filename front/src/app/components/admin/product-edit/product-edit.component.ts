f5930d12fd6274f63bf8379dd975440c5976be18
Adam Jochemczyk <adamjoche@gmail.com>
2021-06-23 15:19:02 +0200

    add select to product edit

--- front/src/app/components/admin/product-edit/product-edit.component.ts
+++ front/src/app/components/admin/product-edit/product-edit.component.ts
@@ -1,41 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CategoryManagerService } from 'src/app/services/categories/category-manager.service';
 import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.css']
+  styleUrls: ['./product-edit.component.css'],
 })
 export class ProductEditComponent implements OnInit {
+  productForm: FormGroup;
+  productId: number | undefined;
+  categoryid: any;
+  categories: any;
 
-  productForm:FormGroup;
-  productId:number|undefined;
-
-  constructor(private router:Router,private formBuilder:FormBuilder,private productService:ProductService) {
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private categoryManager: CategoryManagerService
+  ) {
     //TODO: get by ID from api
     //TODO: get all categories from API with ID
-    console.log(router.getCurrentNavigation()?.extras.state?.data)
+    console.log(router.getCurrentNavigation()?.extras.state?.data);
     let productData = router.getCurrentNavigation()?.extras.state?.data;
-    console.log(productData.name)
-    
+    console.log(productData.name);
+
     this.productForm = this.formBuilder.group({
       name: [productData.name, [Validators.required]],
       description: [productData.description, [Validators.required]],
       unit_price: [productData.unit_price, [Validators.required]],
       image_url: [productData.imgUrl, [Validators.required]],
       available: [productData.available, [Validators.required]],
-      category_id: [null, [Validators.required]],
+      category_id: [this.categoryid, [Validators.required]],
     });
-   }
-
-  ngOnInit(): void {
   }
 
-  updateProduct(){
+  ngOnInit(): void {
+    this.categoryManager.getAllCategories().subscribe((data: any) => {
+      this.categories = data;
+    });
+  }
+
+  updateProduct() {
     if (!this.productForm.valid) return;
     this.productService.updateProduct(this.productForm.value);
   }
-
 }
